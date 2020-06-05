e3b2f285a467c10653a7d5ebd5bcc3ac38130e92
cmatri <connorperkins@cox.net>
2020-06-05 16:08:44 -0600

    Basic algorithm working

---
+++ Assets/Scripts/SinGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinGenerator : ITerrainGenerator
+{
+    public float genVal(float x, float y, float z)
+    {
+        return Mathf.Sin(x * x) + Mathf.Cos(y * y) + z * z - 1;
+        //return Mathf.Sin(x*y + x*z + y*z) + Mathf.Sin(x*y) + Mathf.Sin(y*z) + Mathf.Sin(x*z) - 1;
+        //return 0.5f;
+    }
+}
